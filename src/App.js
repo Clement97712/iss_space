1645cd204bfb594e2e8ab3adfa42d208bcbd2804
Clement97712 <mj.capou@gmail.com>
2019-02-17 16:34:08 +0100

    First update

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
 
   isUpdate = () => {
     axios
-      .get("http://api.open-notify.org/iss-now.json")
+      .get("https://api.open-notify.org/iss-now.json")
       .then(response => {
         console.log(response.data.iss_position);
         this.setState({
@@ -37,7 +37,7 @@ class App extends Component {
   render() {
     const coordonnees = [this.state.latitude, this.state.longitude];
     const attri =
-      "http://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.png";
+      "http://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background{z}/{x}/{y}.png";
     const attri2 =
       'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
 
